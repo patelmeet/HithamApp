25ec7196e02244a2518add94f5290ebdd501fc91
sumedhgupte <guptesumedh@gmail.com>
2017-10-31 02:42:56 +0530

    playlist name and no of songs in playlist

--- src/components/PlayListItem.js
+++ src/components/PlayListItem.js
@@ -22,9 +22,9 @@ export default class PlayListItem extends React.PureComponent {
           <View>
             <View style={styles.rowContainer}>
               <View style={styles.textContainer}>
-                <Text style={styles.id}>{item[PLAYLIST_ID]}</Text>
+                <Text style={styles.id}>{item[PLAYLIST_NAME]}</Text>
                 <Text style={styles.name}
-                  numberOfLines={1}>{item[PLAYLIST_NAME]}</Text>
+                  numberOfLines={1}>{item[PLAYLIST_SONGS].length} songs</Text>
               </View>
             </View>
             <View style={styles.separator}/>
